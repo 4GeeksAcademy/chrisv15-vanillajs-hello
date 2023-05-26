47c1bd538e9312246ab04e3f047026e8f8cf7363
chrisv15 <128597332+chrisv15@users.noreply.github.com>
2023-05-26 10:33:43 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -8,4 +8,66 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let who = [
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "The dog",
+    "My grandma",
+    "His turtle",
+    "My bird",
+    "The dog",
+    "My grandma"
+  ];
+  let action = [
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "ate",
+    "peed",
+    "crushed",
+    "broke",
+    "ate",
+    "ate",
+    "peed"
+  ];
+  let what = [
+    "my homework",
+    "the keys",
+    "the car",
+    "my homework",
+    "the keys",
+    "the car",
+    "my homework",
+    "the keys",
+    "the keys",
+    "the car",
+    "the car"
+  ];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying",
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let elemento = document.getElementById("excusa");
+
+  elemento.textContent =
+    who[Math.floor(Math.random() * 10)] +
+    " " +
+    action[Math.floor(Math.random() * 10)] +
+    " " +
+    what[Math.floor(Math.random() * 10)] +
+    " " +
+    when[Math.floor(Math.random() * 10)] +
+    " ";
 };
